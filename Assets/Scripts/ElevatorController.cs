f220ebe4eeca6cc3f228694b06a55e41b19b2967
Leonardo Simi <dadodugongoso@gmail.com>
2019-04-12 17:16:46 +0200

    finished elevator

---
+++ Assets/Scripts/ElevatorController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour {
+
+    public Player player;
+    public ElevatorController elevatorLink;
+    [SerializeField]
+    //private GameObject elevatorLink;
+    public bool enteredCollision;
+
+
+    // Use this for initialization
+    void Start () {
+        enteredCollision = false;
+        player = GameObject.Find("Player").GetComponent<Player>();
+        elevatorLink = GameObject.Find("elevatorLink").GetComponent<ElevatorController>();
+    }
+
+    void Awake()
+    {
+        Vector2 elevatorLinkPos = elevatorLink.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (enteredCollision == true)
+            {
+                player.transform.position = elevatorLink.transform.position;
+            }
+        }
+
+	}
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+       if (other.tag == "player")
+        {
+            Debug.Log("collisione elevator");
+            enteredCollision = true;
+            /*elevatorLink.enteredCollision = true;
+            if (elevatorLink != null)
+                if (enteredCollision == true)
+                {
+                    {
+                        player.transform.position = elevatorLink.transform.position;
+                    }
+                }*/
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "player")
+        {
+            enteredCollision = false;
+            elevatorLink.enteredCollision = false;
+        }
+    }
+
+}
