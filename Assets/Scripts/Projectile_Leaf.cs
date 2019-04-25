5f9e3b1f0a0c354b1293fb4b277907b040f0112e
Leonardo <dadodugongoso@gmail.com>
2019-04-25 17:24:37 +0200

    Wind Tree minion
    minion + leaf projectile

---
+++ Assets/Scripts/Projectile_Leaf.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_Leaf : MonoBehaviour {
+
+    public float Speed;
+    public Player player;
+    public WindTree windTree;
+
+    private Vector3 dir;
+    private Vector3 pLastPos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        player = GameObject.Find("Player").GetComponent<Player>();
+        windTree = GameObject.Find("WindTree").GetComponent<WindTree>();
+        pLastPos = new Vector3(player.transform.position.x, windTree.transform.position.y - 1, windTree.transform.position.z);
+
+    }
+
+    void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, pLastPos, Speed * Time.deltaTime);
+
+    }
+
+    void getDir()
+    {
+        //dir = 
+        // dir.y = 0.0f; //not changing y dir
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            Destroy(this.gameObject);
+        }
+        else if (other.tag == "player")
+        {
+            Destroy(this.gameObject);
+            StartCoroutine(pDamage());
+        }
+    }
+
+    IEnumerator pDamage()
+    {
+        player.pLives = player.pLives - 1;
+        yield return new WaitForSeconds(2.0f);
+    }
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
+
---
+++ Assets/Scripts/WindTree.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindTree : MonoBehaviour {
+
+    public Player player;
+    public EnemyAI enemyAI;
+    public Rigidbody2D playerRb;
+    public GameObject windArea;
+    [SerializeField]
+    private bool windActive;
+    public GameObject leafProj;
+    public GameObject[] leafInt;
+    private float _canFire = 0.0f;
+    private float _fireRate = 0.75f;
+
+    // Use this for initialization
+    void Start () {
+
+        player = gameObject.GetComponent <Player>();
+        enemyAI = this.GetComponent<EnemyAI>();
+        windArea.SetActive(false);
+        windActive = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (enemyAI.playerFound)
+        {
+            StartCoroutine(windAttack());
+        }
+
+        if (windActive == true)
+        {
+            windArea.SetActive(true);
+            enemyAI.canMove = false;
+            if (Time.time > _canFire)
+            {
+                StartCoroutine(leafShoot());
+                _canFire = Time.time + _fireRate;
+            }
+        }
+        else if (windActive == false)
+        {
+            windArea.SetActive(false);
+            enemyAI.canMove = true;
+        }
+    }
+
+    IEnumerator windAttack()
+    {
+        Debug.Log("start wind");
+        //windArea.SetActive(true);
+        windActive = true;
+        yield return new WaitForSeconds(4f);
+        windActive = false;
+        //windArea.SetActive(false);
+        Debug.Log("stop wind");
+        yield return new WaitForSeconds(4f);
+
+    }
+
+    IEnumerator leafShoot()
+    {
+        Debug.Log("shoot leaves");
+        Instantiate(leafProj, this.transform.position, Quaternion.identity);
+
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    //       GetComponent<Rigidbody> ().AddForce(windDirectionInVector3 * windSpeed);
+}
